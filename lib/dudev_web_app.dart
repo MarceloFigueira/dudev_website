fec09b5d2d7fccc0a176bf86aa6718a3261fbede
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-02-27 21:21:34 -0300

    feat: created base

---
+++ lib/dudev_web_app.dart
@@ -0,0 +1,19 @@
+import 'package:dudev_website/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class DudevWebApp extends StatelessWidget {
+  const DudevWebApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
