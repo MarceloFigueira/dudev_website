c9b8e281e27b16b08f157cdcf860e6ad394ef7e1
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-02-27 22:34:06 -0300

    feat: creating home page structure

--- lib/dudev_web_app.dart
+++ lib/dudev_web_app.dart
@@ -4,16 +4,13 @@ import 'package:flutter/material.dart';
 class DudevWebApp extends StatelessWidget {
   const DudevWebApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Marcelo "Dudev" Figueira',
+      theme: ThemeData.dark(),
+      home: const HomePage(title: 'Marcelo "Dudev" Figueira'),
     );
   }
 }
