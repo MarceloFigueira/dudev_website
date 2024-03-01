bfe8027f2a95be3f7eaad81bb06942af5dd65308
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-02-29 22:08:18 -0300

    feat: created custom drawer

---
+++ lib/src/widgets/drawer_mobile.dart
@@ -0,0 +1,44 @@
+import 'package:dudev_website/src/constants/colors.dart';
+import 'package:dudev_website/src/constants/nav_items.dart';
+import 'package:flutter/material.dart';
+
+class DrawerMobile extends StatelessWidget {
+  const DrawerMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: AppColors.scaffoldBg,
+      child: ListView(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.close),
+              ),
+            ),
+          ),
+          for (int i = 0; i < navIcons.length; i++)
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 30,
+              ),
+              titleTextStyle: TextStyle(
+                color: AppColors.whitePrimary,
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+              onTap: () {},
+              leading: Icon(navIcons[i]),
+              title: Text(navTiles[i]),
+            )
+        ],
+      ),
+    );
+  }
+}
