8eb4cbe49eb59d538ebfffe9fa859b02ec521b31
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-02-29 22:08:05 -0300

    feat: added Icons to nav_items

--- lib/src/constants/nav_items.dart
+++ lib/src/constants/nav_items.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/material.dart';
+
 List<String> navTiles = [
   "Home",
   "Skills",
   "Projects",
   "Blog",
   "Contact",
+];
+
+List<IconData> navIcons = [
+  Icons.home,
+  Icons.android_outlined,
+  Icons.back_hand,
+  Icons.web,
+  Icons.quick_contacts_mail,
 ];
