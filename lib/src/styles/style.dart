807b2cabf2eb677979644d6dd191c045116ddc0c
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-02-29 21:11:38 -0300

    feat: created style

---
+++ lib/src/styles/style.dart
@@ -0,0 +1,11 @@
+import 'package:dudev_website/src/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+BoxDecoration KHeaderDecoration = BoxDecoration(
+    gradient: const LinearGradient(
+      colors: [
+        Colors.transparent,
+        AppColors.bgLight1,
+      ],
+    ),
+    borderRadius: BorderRadius.circular(100));
