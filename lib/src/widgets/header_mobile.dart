f1d7aff5002180b738d8eac7f9235c65d7b206f8
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-02-29 21:11:57 -0300

    feat: created header mobile and web

---
+++ lib/src/widgets/header_mobile.dart
@@ -0,0 +1,40 @@
+import 'package:dudev_website/src/styles/style.dart';
+import 'package:dudev_website/src/widgets/site_logo.dart';
+import 'package:flutter/material.dart';
+
+class HeaderMobile extends StatelessWidget {
+  const HeaderMobile({
+    super.key,
+    this.onLogoTap,
+    this.onMenuTap,
+  });
+
+  final VoidCallback? onLogoTap;
+  final VoidCallback? onMenuTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 50,
+        margin: const EdgeInsets.fromLTRB(40, 5, 20, 5),
+        decoration: KHeaderDecoration,
+        child: Row(
+          children: [
+            SiteLogo(
+              onTap: () {
+                onLogoTap;
+              },
+            ),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  onMenuTap;
+                },
+                icon: const Icon(Icons.menu)),
+            const SizedBox(
+              width: 16,
+            )
+          ],
+        ));
+  }
+}
---
+++ lib/src/widgets/header_web.dart
@@ -0,0 +1,42 @@
+import 'package:dudev_website/src/constants/colors.dart';
+import 'package:dudev_website/src/constants/nav_items.dart';
+import 'package:dudev_website/src/styles/style.dart';
+import 'package:dudev_website/src/widgets/site_logo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HeaderWeb extends StatelessWidget {
+  const HeaderWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      width: double.maxFinite,
+      decoration: KHeaderDecoration,
+      child: Row(
+        children: [
+          SiteLogo(
+            onTap: () {},
+          ),
+          const Spacer(),
+          for (int i = 0; i < navTiles.length; i++)
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  navTiles[i],
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.whitePrimary),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
