ce1f7f1f69e94544913ffe995bf06ca6a4d36c47
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-03-01 23:08:14 -0300

    refactor: update

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:dudev_website/src/constants/colors.dart';
-import 'package:dudev_website/src/constants/nav_items.dart';
+import 'package:dudev_website/src/constants/sizes.dart';
 import 'package:dudev_website/src/widgets/drawer_mobile.dart';
-
 import 'package:dudev_website/src/widgets/header_mobile.dart';
+import 'package:dudev_website/src/widgets/header_web.dart';
+import 'package:dudev_website/src/widgets/main_web.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -21,44 +21,52 @@ class _MyHomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        key: scaffoldKey,
-        backgroundColor: AppColors.scaffoldBg,
-        endDrawer: DrawerMobile(),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            //MAIN
-            //const HeaderWeb(),
-            HeaderMobile(
-              onLogoTap: () {},
-              onMenuTap: () {
-                scaffoldKey.currentState?.openEndDrawer();
-              },
-            ),
-            //SKILLS
-            Container(
-              height: 500,
-              width: double.maxFinite,
-              color: Colors.blueGrey,
-            ),
-            //PROJECTS
-            Container(
-              height: 500,
-              width: double.maxFinite,
-            ),
-            //CONTACT
-            Container(
-              height: 500,
-              width: double.maxFinite,
-              color: Colors.blueGrey,
-            ),
-            //FOOTER
-            Container(
-              height: 500,
-              width: double.maxFinite,
-            )
-          ],
-        ));
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: AppColors.scaffoldBg,
+          endDrawer: constraints.maxWidth >= kMinWebWidth
+              ? null
+              : const DrawerMobile(),
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              //MAIN
+              if (constraints.maxWidth >= kMinWebWidth)
+                const HeaderWeb()
+              else
+                HeaderMobile(
+                  onLogoTap: () {},
+                  onMenuTap: () {
+                    scaffoldKey.currentState?.openEndDrawer();
+                  },
+                ),
+              const MainWeb(),
+
+              //SKILLS
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              //PROJECTS
+              Container(
+                height: 500,
+                width: double.maxFinite,
+              ),
+              //CONTACT
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              //FOOTER
+              Container(
+                height: 500,
+                width: double.maxFinite,
+              )
+            ],
+          ));
+    });
   }
 }
