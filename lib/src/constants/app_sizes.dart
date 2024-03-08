8936883ad10564480d8104b7275965799c7eb3e7
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-03-07 23:26:49 -0300

    feat: add app sizes

---
+++ lib/src/constants/app_sizes.dart
@@ -0,0 +1,21 @@
+class AppSizes {
+  AppSizes._();
+
+  /// Largura minima da versão web
+  static const double minWebWidth = 600;
+
+  /// Largura maxima da versão web
+  static const double maxWebWidth = 1416;
+
+  /// Define tamanho para `0.0`
+  static const double zero = 0.0;
+
+  /// Define tamanho para `1.0`
+  static const double s1 = 1.0;
+
+  /// Define tamanho para `20.0`
+  static const double s20 = 20.0;
+
+  /// Define tamanho para `48.0`
+  static const double s48 = 48.0;
+}
