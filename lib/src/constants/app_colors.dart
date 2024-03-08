10b8fbd708b75c011224eda92bc9e45fe8bc9651
Marcelo Figueira <marcelo.pereira@rarolabs.com.br>
2024-03-07 23:26:39 -0300

    feat: add app colors

---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //New Colors
+  static const Color lightBackground = Color(0xffFBFBFB);
+  static const Color darkBackground = Color(0xff252734);
+  static const Color primaryColor = Color(0xff445964);
+  static const Color darkGrey = Color(0xff263138);
+  static const Color softGrey = Color(0xffDFDFDF);
+  static const Color whitePrimary = Color(0xffeaeaeb);
+  static const Color whiteSecundary = Color(0xffc8c9ce);
+
+  //Old colors
+
+  static const Color bgLight1 = Color(0xff333646);
+  static const Color bgLight2 = Color(0xff424657);
+  static const Color textFieldBg = Color(0xffC8C9CE);
+  static const Color hintDark = Color(0xff666874);
+  static const Color yellowSecondary = Color(0xffffc25c);
+  static const Color yellowPrimary = Color(0xffffaf29);
+}
